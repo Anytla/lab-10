068a0fde32df9112f253e061a55217d01d6c18a2
Anytla <113827865+Anytla@users.noreply.github.com>
2022-12-04 22:54:35 +0200

    Update Program.cs

--- lab 10(2)/Program.cs
+++ lab 10(2)/Program.cs
@@ -1,49 +1,39 @@
-﻿namespace OOP.Reflection.BlackBoxInteger
-{
-    public class BlackBoxInteger
-    {
-        private static int DefaultValue = 0;
-
-        private int innerValue;
-
-        private BlackBoxInteger(int innerValue)
-        {
-            this.innerValue = innerValue;
-        }
-
-        private BlackBoxInteger()
-        {
-            this.innerValue = DefaultValue;
-        }
-
-        private void Add(int addend)
-        {
-            this.innerValue += addend;
-        }
-
-        private void Subtract(int subtrahend)
-        {
-            this.innerValue -= subtrahend;
-        }
-
-        private void Multiply(int multiplier)
-        {
-            this.innerValue *= multiplier;
-        }
-
-        private void Divide(int divider)
-        {
-            this.innerValue /= divider;
-        }
-
-        private void LeftShift(int shifter)
-        {
-            this.innerValue <<= shifter;
-        }
-
-        private void RightShift(int shifter)
-        {
-            this.innerValue >>= shifter;
-        }
-    }
-}
+using System;
+	using System.Reflection;
+	
+	class Ex2
+	{
+	    static void Main()
+	    {
+	        var BlackBox = new BlackBoxInt(); // об'єкт для якого будуть викликатись методи
+	
+	        while (true)
+	        {
+	            string?[]? action = Console.ReadLine()?.Split('_'); // зчитування команди зі значенням, розбиваючи на підстроки
+	
+	            if (action != null) // якщо зчитано не пусту строку
+	                if (action[0]?.ToLower() == "end") break; // якщо кінець вводу
+	                else if (action[0] != null && action[1] != null) // чи надано правильну кількість вхідних даних
+	                {
+	                    var method = typeof(BlackBoxInt).GetMethod // повертається метод в вигляді MethodInfo(var = MethodInfo?)
+	                        (
+	                            action[0]!, // назва метода (Add, Subtract...)
+	                            BindingFlags.Instance | // шукаються методи
+	                            BindingFlags.NonPublic | // шукаються не публічні члени класу
+	                            BindingFlags.Public // шукаються публічні члени класу
+	                        );
+	
+	                    var result = method?.Invoke // виклакається метод і записується в result (var = Object?)
+	                        (
+	                            BlackBox, // об'єкт для якого викликається даний метод
+	                            parameters: new object[] { Convert.ToInt32(action[1]) } // передаються параметри для метода
+	                        );
+	
+	                    Console.WriteLine(result); // вивід результата метода
+	                }
+	        }
+	
+	
+	        Console.WriteLine();
+	    }
+	}
